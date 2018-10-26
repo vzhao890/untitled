28ea5a10dbf12153a566c6a328407aa6ddd21714
BT_1N3_22@bktech.local <BT_1N3_22@bktech.local>
2018-10-26 09:39:10 -0400

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,40 @@
+/*
+public class JackOLantern
+
+{
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+    private String e;
+
+    public static String[][] constructA(String a,String b,String c,String d,String e)
+    {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        this.d=d;
+        this.e=e;
+    }
+
+    public String getA() {
+        return this.a;
+    }
+
+    public String getB() {
+        return this.b;
+    }
+
+    public String getC() {
+        return this.c;
+    }
+
+    public String getD() {
+        return this.d;
+    }
+
+    public String getE() {
+        return this.e;
+    }
+}
+*/
---
+++ src/Runner.java
@@ -0,0 +1,17 @@
+public class Runner
+{
+    public static void main(String[][] args)
+    {
+        //pumpkin=new ConstructorA(args);
+        //System.out.println(pumpkin);
+        int[][] bak={{1,2,3},{4,5,6},{7,8,0}};
+        for(int i=0;i<bak.length;i++)
+        {
+            for(int k=0;k<bak[i].length;k++)
+            {
+                System.out.println(bak[i][k]);
+            }
+        }
+    }
+
+}
